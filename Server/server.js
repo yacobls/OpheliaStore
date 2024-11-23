dfaef771c2c06f20385e5be991a3d1e82ca2325e
yacobls <yacob.lamrodo@gmail.com>
2024-11-23 13:59:10 +0700

    Add Shipping

---
+++ Server/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDatabase from "./config/MongoDb.js";
+import ImportData from "./DataImport.js";
+import productRoute from "./Routes/ProductRoutes.js";
+import { errorHandler, notFound } from "./Middleware/Errors.js";
+import userRouter from "./Routes/UserRoutes.js";
+
+dotenv.config();
+connectDatabase();
+const app = express();
+app.use(express.json());
+
+// API
+app.use("/api/import", ImportData);
+app.use("/api/products", productRoute);
+app.use("/api/users", userRouter);
+
+// ERROR HANDLER
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 1000;
+
+app.listen(PORT, console.log(`Server run in port ${PORT}`));
