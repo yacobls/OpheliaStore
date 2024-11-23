fa6b20abfee4a4258ea2dd8d6c9ed966440341c7
yacobls <yacob.lamrodo@gmail.com>
2024-11-23 14:31:04 +0700

    Payment Screen

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import Header from "../components/Header";
 import { saveShippingAddress } from "../Redux/Actions/cartActions";
 
-const ShippingScreen = ({ history}) => {
+const ShippingScreen = ({ history }) => {
   window.scrollTo(0, 0);
 
   const cart = useSelector((state) => state.cart);
